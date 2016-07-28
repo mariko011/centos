04c2b599eabecf37995e2c86f4d40c0f657cd928
Tianon Gravi <admwiggin@gmail.com>
2016-07-28 10:10:44 -0700

    Start hacking out some license information too

--- .scripts/gather-dpkg.sh
+++ .scripts/gather-dpkg.sh
@@ -45,6 +45,54 @@ for src in "${sortedSources[@]}"; do
 	for bin in ${packages[$src]}; do
 		echo '- `'"$bin"'`'
 	done
+
+	# parse /usr/share/doc/BIN/copyright
+	licenses=()
+	for bin in ${packages[$src]}; do
+		# https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
+		# http://dep.debian.net/deps/dep5/
+		binPkg="${bin%%=*}"
+		binPkgOnly="${binPkg%%:*}"
+		f=
+		for try in \
+			"/usr/share/doc/$bin/copyright" \
+			"/usr/share/doc/$binPkg/copyright" \
+			"/usr/share/doc/$binPkgOnly/copyright" \
+		; do
+			if [ -f "$try" ]; then
+				f="$try"
+				break
+			fi
+		done
+		if [ -z "$f" ]; then
+			echo >&2
+			echo >&2 "**WARNING:** '/usr/share/doc/$binPkgOnly/copyright' is missing!"
+			echo >&2
+			continue
+		fi
+		IFS=$'\n'
+		licenses+=( $(awk -F ':[ \t]+' '$1 == "License" && NF > 1 { gsub(/^License:[ \t]+/, ""); print }' "$f") )
+		licenses+=( $(grep -oE '/usr/share/common-licenses/[0-9a-zA-Z_.+-]+' "$f" | cut -d/ -f5-) )
+		unset IFS
+	done
+	if [ "${#licenses[@]}" -gt 0 ]; then
+		IFS=$'\n'
+		licenses=( $(echo "${licenses[*]}" | sed -r -e 's/ (and|or) /\n/g' -e 's/[.]+$//' | sort -u) )
+		unset IFS
+		echo
+		echo 'Licenses:'
+		echo
+		for lic in "${licenses[@]}"; do
+			echo '- `'"$lic"'`'
+		done
+		echo
+	else
+		echo
+		echo '**WARNING:** unable to detect licenses! (package likely not compliant with DEP-5)'
+		echo '  If source is available (seen below), check the contents of `debian/copyright` within it.'
+		echo
+	fi
+
 	if aptSource="$(apt-get source -qq --print-uris "$src" 2>/dev/null)" && [ -n "$aptSource" ]; then
 		echo
 		echo 'Source:'
