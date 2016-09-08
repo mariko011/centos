9c21c32a791e235451dcedd8e19ff7869bb93811
Tianon Gravi <admwiggin@gmail.com>
2016-09-08 13:05:59 -0700

    Ignore failure to remove containers in "scan-local.sh"

--- scan-local.sh
+++ scan-local.sh
@@ -8,7 +8,7 @@ image="$1"; shift
 docker build --pull -t repo-info:local -q -f Dockerfile.local . > /dev/null
 
 name="repo-info-local-$$-$RANDOM"
-trap "docker rm -f '$name-data' '$name' > /dev/null" EXIT
+trap "docker rm -f '$name-data' '$name' > /dev/null || :" EXIT
 
 docker create \
 	--name "$name-data" \
